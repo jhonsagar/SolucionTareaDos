b0c0b1efcea82d97ba7e48edc7cf0b09a7a652b5
Jhon Jairo <jhonsagar9804@gmail.com>
2022-06-18 12:44:34 -0500

    Hice el primer Ejercio

--- src/main/java/com/mycompany/tareadossolucion/TareaDosSolucion.java
+++ src/main/java/com/mycompany/tareadossolucion/TareaDosSolucion.java
@@ -4,6 +4,7 @@
  */
 
 package com.mycompany.tareadossolucion;
+import java.util.Scanner;
 
 /**
  *
@@ -12,6 +13,43 @@ package com.mycompany.tareadossolucion;
 public class TareaDosSolucion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner op = new Scanner(System.in);
+        //los hice con double para probar con los decimales tambien
+        double Num1;
+        double Num2;
+        System.out.println("Ingrese un Numero :");
+        Num1 = op.nextDouble();
+        System.out.println("Ingrese un Numero :");
+        Num2 = op.nextDouble();
+        Suma(Num1,Num2);
+        Resta(Num1,Num2);
+        Multiplicacion(Num1,Num2);
+        Division(Num1,Num2);
+        
+
     }
+    public static void Suma (double N1, double N2){
+        double resultado = N1+N2;
+        System.out.println("La suma es :"+resultado);
+        }
+    public static void Resta (double N1, double N2){
+        double resultado = N1-N2;
+        System.out.println("La resta es :"+resultado);
+        }
+    public static void Multiplicacion (double N1, double N2){
+        double resultado = N1*N2;
+        System.out.println("La Multiplicacion es :"+resultado);
+        }
+    public static void Division (double N1, double N2){
+        if(N2<=0){
+            System.out.println("Error, no puede dividir entre 0 o con un número negativo");
+        }else{
+            double resultado = N1/N2;
+            System.out.println("El resultado de la division es :"+resultado);
+        }
+        
+        }
+    
+    
+    
 }
